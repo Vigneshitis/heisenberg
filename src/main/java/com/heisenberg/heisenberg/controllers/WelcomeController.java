e46a29dd2b362fa3d0c38fff9f4bb2f7beb848a3
Vignesh B <vignesh200356@gmail.com>
2026-07-25 19:59:46 +0530

    Added WelcomeController in dev3 branch

---
+++ src/main/java/com/heisenberg/heisenberg/controllers/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.heisenberg.heisenberg.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to Spring Boot!";
+    }
+}
