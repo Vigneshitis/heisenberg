03bc22dc767df0b2ffc24aa32da8236353361719
Vignesh B <vignesh200356@gmail.com>
2026-07-25 19:48:01 +0530

    Initial commit for Spring Boot project

---
+++ src/main/java/com/heisenberg/heisenberg/HeisenbergApplication.java
@@ -0,0 +1,14 @@
+package com.heisenberg.heisenberg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HeisenbergApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeisenbergApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heisenberg/heisenberg/HeisenbergApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heisenberg.heisenberg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeisenbergApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
